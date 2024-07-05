505becd3975f285fb4387e76cf7216849c26c3f0
ling0523 <18356201582@163.com>
2024-07-05 17:40:50 +0800

    打包

--- examples/App.vue
+++ examples/App.vue
@@ -12,7 +12,7 @@ const editor = ref<AlexEditor | null>(null)
 
 onMounted(() => {
 	editor.value = new AlexEditor('#editor', {
-		value: '<p><a href="#">这是一个连接</a></p><p>333<video style="width:200px;" autoplay controls src="https://video.699pic.com/videos/92/68/18/a_66840022136f517199268180811.mp4" /></p><table><tbody><tr><td><br></td><td><br></td><td><br></td><td><br></td><td><br></td></tr><tr><td><div>333</div></td><td><br></td><td><br></td><td><br></td><td><br></td></tr><tr><td><br></td><td><br></td><td><br></td><td><br></td><td><br></td></tr><tr><td><br></td><td><br></td><td><br></td><td><br></td><td><br></td></tr><tr><td><br></td><td><br></td><td><br></td><td><br></td><td><br></td></tr></tbody></table><p><span style="color:red;"><span>3</span></span></p>',
+		value: '<p><a href="#">这是一个连接</a>33444</p><p>333<video style="width:200px;" autoplay controls src="https://video.699pic.com/videos/92/68/18/a_66840022136f517199268180811.mp4" /></p><table><tbody><tr><td><br></td><td><br></td><td><br></td><td><br></td><td><br></td></tr><tr><td><div>333</div></td><td><br></td><td><br></td><td><br></td><td><br></td></tr><tr><td><br></td><td><br></td><td><br></td><td><br></td><td><br></td></tr><tr><td><br></td><td><br></td><td><br></td><td><br></td><td><br></td></tr><tr><td><br></td><td><br></td><td><br></td><td><br></td><td><br></td></tr></tbody></table><p><span style="color:red;"><span>3</span></span></p>',
 		allowPasteHtml: true,
 		extraKeepTags: ['svg', 'circle']
 	})
