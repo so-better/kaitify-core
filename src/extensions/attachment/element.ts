42fa58178cfa2e8d25f8d59a90e9ae6e5477d514
so-better <18356201582@163.com>
2026-03-28 14:24:38 +0800

    feat：优化附件样式

--- src/extensions/attachment/element.ts
+++ src/extensions/attachment/element.ts
@@ -1,35 +1,35 @@
 export const ATTACHMENT_NODE_TAG = 'kaitify-attachment'
 
 class AttachmentElement extends HTMLElement {
-  constructor() {
-    super()
-  }
+	constructor() {
+		super()
+	}
 
-  /**
-   * dom挂载，首次渲染
-   */
-  connectedCallback() {
-    this.innerHTML = `<span><span is-icon></span><span is-text></span></span>`
-    this.querySelector('span[is-icon]')!.setAttribute('style', `background-image:url(${this.getAttribute('data-icon')})`)
-    this.querySelector('span[is-text]')!.textContent = this.getAttribute('data-text')
-  }
+	/**
+	 * dom挂载，首次渲染
+	 */
+	connectedCallback() {
+		this.innerHTML = `<span is-icon></span><span is-text></span>`
+		this.querySelector('span[is-icon]')!.setAttribute('style', `background-image:url(${this.getAttribute('data-icon')})`)
+		this.querySelector('span[is-text]')!.textContent = this.getAttribute('data-text')
+	}
 
-  /**
-   * 属性更新时更新内容
-   */
-  attributeChangedCallback(name: string, _: string, newValue: string) {
-    if (name == 'data-icon') {
-      const icon = this.querySelector('span[is-icon]')
-      if (icon) (icon as HTMLElement).style.backgroundImage = `url(${newValue})`
-    } else if (name == 'data-text') {
-      const text = this.querySelector('span[is-text]')
-      if (text) text.textContent = newValue
-    }
-  }
+	/**
+	 * 属性更新时更新内容
+	 */
+	attributeChangedCallback(name: string, _: string, newValue: string) {
+		if (name == 'data-icon') {
+			const icon = this.querySelector('span[is-icon]')
+			if (icon) (icon as HTMLElement).style.backgroundImage = `url(${newValue})`
+		} else if (name == 'data-text') {
+			const text = this.querySelector('span[is-text]')
+			if (text) text.textContent = newValue
+		}
+	}
 
-  static get observedAttributes() {
-    return ['data-text', 'data-icon']
-  }
+	static get observedAttributes() {
+		return ['data-text', 'data-icon']
+	}
 }
 
 customElements.define(ATTACHMENT_NODE_TAG, AttachmentElement)
